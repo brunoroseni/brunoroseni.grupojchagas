863dd28fa04e13489c620baf890ce46ff4139b90
brunoroseni <brunoroseni@hotmail.com>
2025-04-25 09:55:47 -0400

    add botao scroll to top; add scroll behavior

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,28 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item-story')
     document.querySelector('.slide-story').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+
+
+
+
+
+// Get the button:
+let topBtn = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    top.Btn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
